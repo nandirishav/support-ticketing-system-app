034078d6e23853d6b091bb99d796bafbd988349c
Rishav Nandi <rishav@rhombuz.io>
2022-05-22 16:43:23 +0530

    added db connection,userController, errorMiddleware,userRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,28 @@
-console.log("server running");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const userRoutes = require("./routes/userRoutes");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
+//Connect to db
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello, Welcome to the Support desk api" });
+});
+
+//Routes
+app.use("/api/users", userRoutes);
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}`);
+});

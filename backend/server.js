7db79fdb64cb5e35004f4cdc850ccf72a7609384
Rishav Nandi <rishav@rhombuz.io>
2022-05-29 16:14:55 +0530

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
@@ -6,6 +7,7 @@ const userRoutes = require("./routes/userRoutes");
 const ticketRoutes = require("./routes/ticketRoutes");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
+const { path } = require("express/lib/application");
 
 //Connect to db
 connectDB();
@@ -15,13 +17,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello, Welcome to the Support desk api" });
-});
-
 //Routes
 app.use("/api/users", userRoutes);
 app.use("/api/tickets", ticketRoutes);
+
+//Serve frontend
+if (process.env.NODE_ENV === "production") {
+  //set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.json({ message: "Hello, Welcome to the Support desk api" });
+  });
+}
 
 app.use(errorHandler);
 
